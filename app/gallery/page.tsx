af3dcb9353eb989d79581adce7ea736e06696a85
kiranpate1 <kp8568@gmail.com>
2026-01-29 03:09:46 -0500

    test

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,12 +1,5 @@
-import { getNotionEvents } from "@/app/actions/getEvents";
 import GalleryWrapper from "./galleryWrapper";
 
 export default async function Home() {
-  const events = await getNotionEvents();
-
-  return (
-    <div className="">
-      <GalleryWrapper />
-    </div>
-  );
+  return <GalleryWrapper />;
 }
