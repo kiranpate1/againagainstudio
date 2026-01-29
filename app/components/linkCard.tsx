aaecc0e89c7681a0bdb76b4582a89fb702442e4a
kiranpate1 <kp8568@gmail.com>
2026-01-29 18:11:31 -0500

    test

--- app/components/linkCard.tsx
+++ app/components/linkCard.tsx
@@ -41,12 +41,12 @@ export default function LinkCard({ title, date, link, colors }: Props) {
       </svg>
       <div className="flex-1 paragraph text-balance">
         <p className="inline">{title}</p>{" "}
-        <div
-          className="-rotate-45"
+        <span
+          className="inline-block -rotate-45"
           style={{ display: date ? "inline-block" : "none" }}
         >
           →
-        </div>
+        </span>
       </div>
       <p className="paragraph">
         {date &&
