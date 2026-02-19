2b01b72abfdfd3148051537cc0a305422339182d
kiranpate1 <kp8568@gmail.com>
2026-02-18 21:22:23 -0500

    final updates to mobile home

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const events = await getNotionEvents();
 
   return (
-    <div className="relative inset-0 w-screen h-screen min-h-[700px] lg:min-h-[800px] flex flex-col gap-8 md:gap-14 items-start justify-start p-[72px_16px_108px_16px] lg:p-[92px_40px_92px_40px]">
+    <div className="relative inset-0 w-screen h-screen min-h-[700px] lg:min-h-[800px] flex flex-col gap-8 md:gap-14 items-start justify-start px-4 pt-18 pb-27 lg:px-10 lg:pt-23 lg:pb-23">
       <div className="md:w-[80%] flex flex-col items-start gap-2">
         <h1 className="heading-large">
           Again Again is a creative studio in Toronto that curates social events
@@ -20,9 +20,9 @@ export default async function Home() {
           Learn more about the space
         </a>
       </div>
-      <div className="flex-1 w-full flex flex-col gap-3 items-stretch justify-start">
-        <p className="paragraph uppercase">Upcoming Events</p>
-        <div className="flex-1 flex gap-6 h-full overflow-x-scroll">
+      <div className="flex-1 w-[calc(100%+32px)] lg:w-full -translate-x-4 lg:translate-x-0 flex flex-col gap-3 items-stretch justify-start">
+        <p className="paragraph uppercase pl-4 lg:pl-0">Upcoming Events</p>
+        <div className="flex-1 flex gap-6 h-full px-4 overflow-x-scroll">
           {events.map((event: any, index: number) => (
             <a
               key={event.id}
@@ -43,7 +43,7 @@ export default async function Home() {
                   </div>,
                 )}
               </div>
-              <div className="absolute inset-[auto_0_0_0] px-6 py-4 lg:py-6 flex flex-col items-start justify-end gap-3">
+              <div className="absolute z-2 inset-[auto_0_0_0] px-6 py-4 lg:py-6 flex flex-col items-start justify-end gap-3">
                 <p className="caption">{formatEventDate(event.date)}</p>
                 <div className="flex flex-col gap-1">
                   <h2 className="paragraph text-balance">{event.eventName}</h2>
@@ -59,7 +59,7 @@ export default async function Home() {
           href="https://luma.com/againagain"
           target="_blank"
           rel="noopener noreferrer"
-          className="paragraph text-(--charm) cursor hover:opacity-70"
+          className="paragraph text-(--charm) cursor hover:opacity-70 pl-4 lg:pl-0"
         >
           View all events
         </a>
--- app/shapes.tsx
+++ app/shapes.tsx
@@ -9,212 +9,276 @@ export const shapes: SvgElement[] = [
     id: "shape-1",
     name: "Example Shape 1",
     svg: (imageElement) => (
-      <svg
-        className="absolute inset-0 w-full h-full"
-        viewBox="0 0 463 463"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <clipPath id="clip-shape-1">
-            <path d="M463 36.9179L463 411.923L445.378 449.157L424.945 462.999L46.2716 463L22.0547 457.512L2.65688e-06 428.078L2.76463e-05 44.1176L27.5 25L38.7772 5.79756L52.6251 0.00111552L427.107 -1.05373e-06L463 36.9179Z" />
-          </clipPath>
-          <linearGradient
-            id="scrim-gradient-1"
-            x1="0%"
-            y1="0%"
-            x2="0%"
-            y2="100%"
-          >
-            <stop offset="0%" stopColor="black" stopOpacity="0" />
-            <stop offset="100%" stopColor="black" stopOpacity="0.7" />
-          </linearGradient>
-        </defs>
-        <path
-          d="M463 36.9179L463 411.923L445.378 449.157L424.945 462.999L46.2716 463L22.0547 457.512L2.65688e-06 428.078L2.76463e-05 44.1176L27.5 25L38.7772 5.79756L52.6251 0.00111552L427.107 -1.05373e-06L463 36.9179Z"
-          fill="var(--copper)"
-        />
+      <>
+        <svg className="absolute w-0 h-0" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <clipPath id="clip-shape-1" clipPathUnits="objectBoundingBox">
+              <path
+                transform="scale(0.00216, 0.00216)"
+                d="M463 36.9179L463 411.923L445.378 449.157L424.945 462.999L46.2716 463L22.0547 457.512L2.65688e-06 428.078L2.76463e-05 44.1176L27.5 25L38.7772 5.79756L52.6251 0.00111552L427.107 -1.05373e-06L463 36.9179Z"
+              />
+            </clipPath>
+          </defs>
+        </svg>
         {imageElement && (
-          <foreignObject
-            x="0"
-            y="0"
-            width="463"
-            height="463"
-            clipPath="url(#clip-shape-1)"
+          <div
+            className="absolute z-1 inset-0 w-full h-full"
+            style={{ clipPath: "url(#clip-shape-1)" }}
           >
             {imageElement}
-          </foreignObject>
+          </div>
         )}
-        <g clipPath="url(#clip-shape-1)">
-          <rect
-            x="0"
-            y="293"
-            width="463"
-            height="170"
-            fill="url(#scrim-gradient-1)"
-            className="origin-bottom group-hover:scale-y-200"
+        <svg
+          className="absolute inset-0 w-full h-full pointer-events-none"
+          viewBox="0 0 463 463"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M463 36.9179L463 411.923L445.378 449.157L424.945 462.999L46.2716 463L22.0547 457.512L2.65688e-06 428.078L2.76463e-05 44.1176L27.5 25L38.7772 5.79756L52.6251 0.00111552L427.107 -1.05373e-06L463 36.9179Z"
+            fill="var(--copper)"
           />
-        </g>
-      </svg>
+        </svg>
+        <svg
+          className="absolute z-2 inset-0 w-full h-full pointer-events-none"
+          viewBox="0 0 463 463"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <clipPath id="clip-shape-1-gradient">
+              <path d="M463 36.9179L463 411.923L445.378 449.157L424.945 462.999L46.2716 463L22.0547 457.512L2.65688e-06 428.078L2.76463e-05 44.1176L27.5 25L38.7772 5.79756L52.6251 0.00111552L427.107 -1.05373e-06L463 36.9179Z" />
+            </clipPath>
+            <linearGradient
+              id="scrim-gradient-1"
+              x1="0%"
+              y1="0%"
+              x2="0%"
+              y2="100%"
+            >
+              <stop offset="0%" stopColor="black" stopOpacity="0" />
+              <stop offset="100%" stopColor="black" stopOpacity="0.7" />
+            </linearGradient>
+          </defs>
+          <g clipPath="url(#clip-shape-1-gradient)">
+            <rect
+              x="0"
+              y="293"
+              width="463"
+              height="170"
+              fill="url(#scrim-gradient-1)"
+              className="origin-bottom group-hover:scale-y-200"
+            />
+          </g>
+        </svg>
+      </>
     ),
   },
   {
     id: "shape-2",
     name: "Example Shape 2",
     svg: (imageElement) => (
-      <svg
-        className="absolute inset-0 w-full h-full"
-        viewBox="0 0 463 463"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <clipPath id="clip-shape-2">
-            <path d="M0 0.000138172L108.474 0.000103629C108.474 11.0229 120.32 15.7483 134.931 15.7483C149.543 15.7483 161.389 9.44898 161.389 6.90862e-05L416.7 0L463 26.7721V429.928L407.44 463H0V0.000138172Z" />
-          </clipPath>
-          <linearGradient
-            id="scrim-gradient-2"
-            x1="0%"
-            y1="0%"
-            x2="0%"
-            y2="100%"
-          >
-            <stop offset="0%" stopColor="black" stopOpacity="0" />
-            <stop offset="100%" stopColor="black" stopOpacity="0.7" />
-          </linearGradient>
-        </defs>
-        <path
-          d="M0 0.000138172L108.474 0.000103629C108.474 11.0229 120.32 15.7483 134.931 15.7483C149.543 15.7483 161.389 9.44898 161.389 6.90862e-05L416.7 0L463 26.7721V429.928L407.44 463H0V0.000138172Z"
-          fill="var(--green-800)"
-        />
+      <>
+        <svg className="absolute w-0 h-0" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <clipPath id="clip-shape-2" clipPathUnits="objectBoundingBox">
+              <path
+                transform="scale(0.00216, 0.00216)"
+                d="M0 0.000138172L108.474 0.000103629C108.474 11.0229 120.32 15.7483 134.931 15.7483C149.543 15.7483 161.389 9.44898 161.389 6.90862e-05L416.7 0L463 26.7721V429.928L407.44 463H0V0.000138172Z"
+              />
+            </clipPath>
+          </defs>
+        </svg>
         {imageElement && (
-          <foreignObject
-            x="0"
-            y="0"
-            width="463"
-            height="463"
-            clipPath="url(#clip-shape-2)"
+          <div
+            className="absolute z-1 inset-0 w-full h-full"
+            style={{ clipPath: "url(#clip-shape-2)" }}
           >
             {imageElement}
-          </foreignObject>
+          </div>
         )}
-        <g clipPath="url(#clip-shape-2)">
-          <rect
-            x="0"
-            y="293"
-            width="463"
-            height="170"
-            fill="url(#scrim-gradient-2)"
-            className="origin-bottom group-hover:scale-y-200"
+        <svg
+          className="absolute inset-0 w-full h-full pointer-events-none"
+          viewBox="0 0 463 463"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 0.000138172L108.474 0.000103629C108.474 11.0229 120.32 15.7483 134.931 15.7483C149.543 15.7483 161.389 9.44898 161.389 6.90862e-05L416.7 0L463 26.7721V429.928L407.44 463H0V0.000138172Z"
+            fill="var(--green-800)"
           />
-        </g>
-      </svg>
+        </svg>
+        <svg
+          className="absolute z-2 inset-0 w-full h-full pointer-events-none"
+          viewBox="0 0 463 463"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <clipPath id="clip-shape-2-gradient">
+              <path d="M0 0.000138172L108.474 0.000103629C108.474 11.0229 120.32 15.7483 134.931 15.7483C149.543 15.7483 161.389 9.44898 161.389 6.90862e-05L416.7 0L463 26.7721V429.928L407.44 463H0V0.000138172Z" />
+            </clipPath>
+            <linearGradient
+              id="scrim-gradient-2"
+              x1="0%"
+              y1="0%"
+              x2="0%"
+              y2="100%"
+            >
+              <stop offset="0%" stopColor="black" stopOpacity="0" />
+              <stop offset="100%" stopColor="black" stopOpacity="0.7" />
+            </linearGradient>
+          </defs>
+          <g clipPath="url(#clip-shape-2-gradient)">
+            <rect
+              x="0"
+              y="293"
+              width="463"
+              height="170"
+              fill="url(#scrim-gradient-2)"
+              className="origin-bottom group-hover:scale-y-200"
+            />
+          </g>
+        </svg>
+      </>
     ),
   },
   {
     id: "shape-3",
     name: "Example Shape 3",
     svg: (imageElement) => (
-      <svg
-        className="absolute inset-0 w-full h-full"
-        viewBox="0 0 463 463"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <clipPath id="clip-shape-3">
-            <path d="M33.5326 0L423.367 2.43871L446.565 22.9327L463 28.6438V436.685L446.747 447.32L423.367 463L33.5326 461.866L13.3721 444.828L0 436.897L0 23.8912H13.8445L33.5326 0Z" />
-          </clipPath>
-          <linearGradient
-            id="scrim-gradient-3"
-            x1="0%"
-            y1="0%"
-            x2="0%"
-            y2="100%"
-          >
-            <stop offset="0%" stopColor="black" stopOpacity="0" />
-            <stop offset="100%" stopColor="black" stopOpacity="0.7" />
-          </linearGradient>
-        </defs>
-        <path
-          d="M33.5326 0L423.367 2.43871L446.565 22.9327L463 28.6438V436.685L446.747 447.32L423.367 463L33.5326 461.866L13.3721 444.828L0 436.897L0 23.8912H13.8445L33.5326 0Z"
-          fill="var(--red-800)"
-        />
+      <>
+        <svg className="absolute w-0 h-0" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <clipPath id="clip-shape-3" clipPathUnits="objectBoundingBox">
+              <path
+                transform="scale(0.00216, 0.00216)"
+                d="M33.5326 0L423.367 2.43871L446.565 22.9327L463 28.6438V436.685L446.747 447.32L423.367 463L33.5326 461.866L13.3721 444.828L0 436.897L0 23.8912H13.8445L33.5326 0Z"
+              />
+            </clipPath>
+          </defs>
+        </svg>
         {imageElement && (
-          <foreignObject
-            x="0"
-            y="0"
-            width="463"
-            height="463"
-            clipPath="url(#clip-shape-3)"
+          <div
+            className="absolute z-1 inset-0 w-full h-full"
+            style={{ clipPath: "url(#clip-shape-3)" }}
           >
             {imageElement}
-          </foreignObject>
+          </div>
         )}
-        <g clipPath="url(#clip-shape-3)">
-          <rect
-            x="0"
-            y="293"
-            width="463"
-            height="170"
-            fill="url(#scrim-gradient-3)"
-            className="origin-bottom group-hover:scale-y-200"
+        <svg
+          className="absolute inset-0 w-full h-full pointer-events-none"
+          viewBox="0 0 463 463"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M33.5326 0L423.367 2.43871L446.565 22.9327L463 28.6438V436.685L446.747 447.32L423.367 463L33.5326 461.866L13.3721 444.828L0 436.897L0 23.8912H13.8445L33.5326 0Z"
+            fill="var(--red-800)"
           />
-        </g>
-      </svg>
+        </svg>
+        <svg
+          className="absolute z-2 inset-0 w-full h-full pointer-events-none"
+          viewBox="0 0 463 463"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <clipPath id="clip-shape-3-gradient">
+              <path d="M33.5326 0L423.367 2.43871L446.565 22.9327L463 28.6438V436.685L446.747 447.32L423.367 463L33.5326 461.866L13.3721 444.828L0 436.897L0 23.8912H13.8445L33.5326 0Z" />
+            </clipPath>
+            <linearGradient
+              id="scrim-gradient-3"
+              x1="0%"
+              y1="0%"
+              x2="0%"
+              y2="100%"
+            >
+              <stop offset="0%" stopColor="black" stopOpacity="0" />
+              <stop offset="100%" stopColor="black" stopOpacity="0.7" />
+            </linearGradient>
+          </defs>
+          <g clipPath="url(#clip-shape-3-gradient)">
+            <rect
+              x="0"
+              y="293"
+              width="463"
+              height="170"
+              fill="url(#scrim-gradient-3)"
+              className="origin-bottom group-hover:scale-y-200"
+            />
+          </g>
+        </svg>
+      </>
     ),
   },
   {
     id: "shape-4",
     name: "Example Shape 4",
     svg: (imageElement) => (
-      <svg
-        className="absolute inset-0 w-full h-full"
-        viewBox="0 0 463 463"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <defs>
-          <clipPath id="clip-shape-4">
-            <path d="M0 40.9735L37.5674 0H417.446C426 24.5 439 28 462.941 37.3648L463 415.489L410.335 463L400.27 458.903L37.5674 463L2.92768e-05 422.122L0 40.9735Z" />
-          </clipPath>
-          <linearGradient
-            id="scrim-gradient-4"
-            x1="0%"
-            y1="0%"
-            x2="0%"
-            y2="100%"
-          >
-            <stop offset="0%" stopColor="black" stopOpacity="0" />
-            <stop offset="100%" stopColor="black" stopOpacity="0.7" />
-          </linearGradient>
-        </defs>
-        <path
-          d="M0 40.9735L37.5674 0H417.446C426 24.5 439 28 462.941 37.3648L463 415.489L410.335 463L400.27 458.903L37.5674 463L2.92768e-05 422.122L0 40.9735Z"
-          fill="var(--copper)"
-        />
+      <>
+        <svg className="absolute w-0 h-0" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <clipPath id="clip-shape-4" clipPathUnits="objectBoundingBox">
+              <path
+                transform="scale(0.00216, 0.00216)"
+                d="M0 40.9735L37.5674 0H417.446C426 24.5 439 28 462.941 37.3648L463 415.489L410.335 463L400.27 458.903L37.5674 463L2.92768e-05 422.122L0 40.9735Z"
+              />
+            </clipPath>
+          </defs>
+        </svg>
         {imageElement && (
-          <foreignObject
-            x="0"
-            y="0"
-            width="463"
-            height="463"
-            clipPath="url(#clip-shape-4)"
+          <div
+            className="absolute z-1 inset-0 w-full h-full"
+            style={{ clipPath: "url(#clip-shape-4)" }}
           >
             {imageElement}
-          </foreignObject>
+          </div>
         )}
-        <g clipPath="url(#clip-shape-4)">
-          <rect
-            x="0"
-            y="293"
-            width="463"
-            height="170"
-            fill="url(#scrim-gradient-4)"
-            className="origin-bottom group-hover:scale-y-200"
+        <svg
+          className="absolute inset-0 w-full h-full pointer-events-none"
+          viewBox="0 0 463 463"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 40.9735L37.5674 0H417.446C426 24.5 439 28 462.941 37.3648L463 415.489L410.335 463L400.27 458.903L37.5674 463L2.92768e-05 422.122L0 40.9735Z"
+            fill="var(--copper)"
           />
-        </g>
-      </svg>
+        </svg>
+        <svg
+          className="absolute z-2 inset-0 w-full h-full pointer-events-none"
+          viewBox="0 0 463 463"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <clipPath id="clip-shape-4-gradient">
+              <path d="M0 40.9735L37.5674 0H417.446C426 24.5 439 28 462.941 37.3648L463 415.489L410.335 463L400.27 458.903L37.5674 463L2.92768e-05 422.122L0 40.9735Z" />
+            </clipPath>
+            <linearGradient
+              id="scrim-gradient-4"
+              x1="0%"
+              y1="0%"
+              x2="0%"
+              y2="100%"
+            >
+              <stop offset="0%" stopColor="black" stopOpacity="0" />
+              <stop offset="100%" stopColor="black" stopOpacity="0.7" />
+            </linearGradient>
+          </defs>
+          <g clipPath="url(#clip-shape-4-gradient)">
+            <rect
+              x="0"
+              y="293"
+              width="463"
+              height="170"
+              fill="url(#scrim-gradient-4)"
+              className="origin-bottom group-hover:scale-y-200"
+            />
+          </g>
+        </svg>
+      </>
     ),
   },
 ];
